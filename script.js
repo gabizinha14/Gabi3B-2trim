3118f5fdbc71d333fc2b5372bd802052946b20a1
Gabi :D <112398986+gabizinha14@users.noreply.github.com>
2024-07-21 19:07:09 -0300

    21/07

--- script.js
+++ script.js
@@ -32,14 +32,14 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "no dia seguinte, marcinha chega ao trabalho e ve a sueli batendo no eder, o que fazer?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                texto: "gritar para eles pararem a briga",
                 afirmacao: "positivo"
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                texto: "bater nos dois",
                 afirmacao: "negativo"
             }
         ]
